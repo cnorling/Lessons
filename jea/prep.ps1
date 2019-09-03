12ec778479f94cc9fa38d817fbdcf1390674eede
salineselin <tersel@live.com>
2019-09-02 18:43:23 -0600

    added user, group, and GMSA account creation

--- jea/prep.ps1
+++ jea/prep.ps1
@@ -12,24 +12,24 @@ All these virtual machines will be setup with hyper-v.
 $accounts = @{
     # create a credential object for your domain admin creds
     domainadmin = @{
-        user = "home.lab\domain admin"
-        password = ConvertTo-SecureString -AsPlainText -Force -String "domainadminpassword"
+        username = "home.lab\administrator"
+        password = ConvertTo-SecureString -AsPlainText -Force -String "Homelab!"
     }
     # create a credential object for your local admin password (to setup all 3 machines)
     localadmin = @{
-        user = "administrator"
-        password = ConvertTo-SecureString -AsPlainText -force -String "localadminpassword"
+        username = "administrator"
+        password = ConvertTo-SecureString -AsPlainText -force -String "Homelab!"
     }
     # create a credential object for bob
     bob = @{
-        user = "home.lab\bob"
+        username = "home.lab\Bob.Saget"
         password = ConvertTo-SecureString -AsPlainText -force -String "Your password sucks bob1"
     }
 }
 $credential = @{
-    domainadmin = new-object pscredential -argumentlist $accounts.domainadmin.user,$accounts.domainadmin.password
-    localadmin = new-object pscredential -argumentlist $accounts.localadmin.user,$accounts.localadmin.password
-    bob = new-object pscredential -argumentlist $accounts.bob.user,$accounts.bob.password
+    domainadmin = new-object pscredential -argumentlist $accounts.domainadmin.username,$accounts.domainadmin.password
+    localadmin = new-object pscredential -argumentlist $accounts.localadmin.username,$accounts.localadmin.password
+    bob = new-object pscredential -argumentlist $accounts.bob.username,$accounts.bob.password
 }
 
 ## setup DOMAIN-1
@@ -37,34 +37,58 @@ $credential = @{
 New-VMFromTemplate -name "DOMAIN-1" -vhdxpath "D:\hyperv\disks\" -vhdxtemplate "D:\hyperv\disks\template.vhdx" -switch "domain" -memory 1
 
 # rename the computer and set a static IP address, and add some windowsfeatures
-Invoke-Command -VMname "DOMAIN-1"-Credential $credential {
+Invoke-Command -VMname "DOMAIN-1"-Credential $credential.localadmin {
     New-NetIPAddress -IPAddress "10.10.10.10" -InterfaceIndex (Get-NetAdapter).interfaceindex -DefaultGateway "10.10.10.1" -PrefixLength 24
     Install-WindowsFeature -Name "RSAT-AD-Tools","DNS","DHCP","AD-Domain-Services"
     Rename-Computer -NewName "DOMAIN-1" -Restart
 }
 
 # create the domain HOME.LAB
-Invoke-Command -VMname "DOMAIN-1" -Credential  $credential {
-    $param = @{
-        creatednsdelegation = $false
-        databasepath = "C:\windows\NTDS"
-        domainmode = "Win2012R2"
-        domainname = "home.lab"
-        domainnetbiosname = "home"
-        forestmode = "Win2012R2"
-        installdns = $true
-        logpath = "C:\windows\NTDS"
-        norebootoncompletion = $false
-        sysvolpath = "C:\Windows\SYSVOL"
-        force = $true
-        safemodeadministratorpassword = $using:accounts.domainadmin.password
-    }
-    Install-ADDSForest @param
+$param = @{
+    creatednsdelegation = $false
+    databasepath = "C:\windows\NTDS"
+    domainmode = "Win2012R2"
+    domainname = "home.lab"
+    domainnetbiosname = "home"
+    forestmode = "Win2012R2"
+    installdns = $true
+    logpath = "C:\windows\NTDS"
+    norebootoncompletion = $false
+    sysvolpath = "C:\Windows\SYSVOL"
+    force = $true
+    safemodeadministratorpassword = $using:accounts.domainadmin.password
+}
+Invoke-Command -VMname "DOMAIN-1" -Credential $credential.localadmin {
+    Install-ADDSForest @using:param
 }
 
-# create a domain account to use
-# create a domain group to use
-# create a GMSA
+# create a domain account, a group, and a GMSA to use ... and a KDS root key so you can make GMSAs before the 10 hour convergence cycle
+# https://social.technet.microsoft.com/Forums/ie/en-US/82617035-254f-4078-baa2-7b46abb9bb71/newadserviceaccount-key-does-not-exist?forum=winserver8gen
+$user = @{
+    name = "Bob.Saget"
+    SamAccountName  = "Bob.Saget"
+    accountpassword = $accounts.bob.password
+    enabled = $true
+    path = "CN=Users,DC=home,DC=lab"
+}
+$group = @{
+    name = "JEA_Group"
+    SamAccountName = "JEA_Group"
+    displayname = "JEA_Group"
+    groupcategory = "Security"
+    groupscope = "Global"
+    path = "CN=Users,DC=home,DC=lab"
+}
+$gmsa = @{
+    name = "JEA_GMSA"
+    dnshostname = "JEA_GMSA.home.lab"
+}
+invoke-command -VMname "DOMAIN-1" -Credential $credential.domainadmin {
+    Add-KdsRootKey –EffectiveTime ((get-date).addhours(-10))
+    New-ADUser @using:user
+    New-ADGroup @using:group
+    New-ADServiceAccount @using:gmsa
+}
 
 ## setup SERVER-1
 # create the VM
