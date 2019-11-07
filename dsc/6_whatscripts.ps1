2c5618aab61170f6f352b12ddb4cf5a187e35f06
salineselin <tersel@live.com>
2019-11-06 20:52:47 -0700

    added another document

---
+++ dsc/6_whatscripts.ps1
@@ -0,0 +1,2 @@
+# these dsc resources are just abstractions of actual scripts. Where's the actual powershell?
+code 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\PSDesiredStateConfiguration\DSCResources\MSFT_ServiceResource\MSFT_ServiceResource.psm1'
